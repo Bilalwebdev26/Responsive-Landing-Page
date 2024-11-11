80aefe540a75c663a0f115abbc9805174c5a601a
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-11-11 09:17:53 -0800

    Header update

---
+++ Landing Page/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { navItems } from '../constants'
+import logo from "../assets/logo.png"
+const Navbar = () => {
+  return (
+   <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative text-sm">
+        <div className="flex justify-center items-center">
+            <div className="flex items-center flex-shrink-0">
+                <img className='h-10' src={logo} alt="logo"/>
+                <span className='text-xl tracking-tight'>VirtuaLiR</span>
+            </div>
+
+            <ul className='hidden lg:flex ml-14 space-x-12'>
+                   {navItems.map((items,index)=>(
+                       <li key={index}>
+                          <a href={items.href}>{items.label}</a>
+                       </li>
+                   ))}
+            </ul>
+
+        </div>
+      </div>
+   </nav>
+  )
+}
+
+export default Navbar
